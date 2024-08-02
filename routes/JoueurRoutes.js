d87ed5f94ea93be879d07e8f64f8f0e89fcf0ef5
Christoooophe <christophealfier02@gmail.com>
2024-08-02 11:08:15 +0200

    feature / ajout méthode put joueur et id incrémenté auto

--- routes/JoueurRoutes.js
+++ routes/JoueurRoutes.js
@@ -40,8 +40,12 @@ router.get('/joueurs/:id', (req, res) => {
 
 // Ajout nouveau joueur
 router.post('/ajout-joueur', (req, res) => {
-    const nouveauJoueur = req.body
-    console.log(req.body)
+    const nouveauJoueur = {
+        id: Date.now(),
+        gamerTag: req.body.gamerTag,
+        progression: 0,
+        score: 0
+    }
 
     fs.readFile(joueurJSONPath, 'utf8', (err, data) => {
         if (err) {
@@ -66,5 +70,39 @@ router.post('/ajout-joueur', (req, res) => {
     })
 });
 
+// Update du joueur
+router.put('/joueur/update/:id', (req,res) => {
+    fs.readFile(joueurJSONPath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Erreur de fichier : ', err);
+            res.status(500).send('Erreur interne du serveur');
+            return;
+        }
+
+        const listeJoueurs = JSON.parse(data);
+        const joueur = listeJoueurs.find(joueur => joueur.id === parseInt(req.params.id))
+
+        if (!joueur) {
+            return res.status(404).send('Joueur non trouvé');
+        }
+        const joueurUpdate = {
+            id: joueur.id,
+            gamerTag: joueur.gamerTag,
+            progression: req.body.progression,
+            points : req.body.points,
+        }
+        listeJoueurs.splice(joueur, 1);
+        listeJoueurs.push(joueurUpdate);
+        fs.writeFile(joueurJSONPath, JSON.stringify(listeJoueurs), err => {
+            if (err) {
+                console.error('Erreur lors de l\'écriture dans le fichier JSON :', err);
+                res.status(500).send("Erreur interne du serveur");
+                return;
+            }
+
+            res.status(201).send("Joueur modifié avec succès")
+        })
+    })
+})
 
 module.exports = router;
